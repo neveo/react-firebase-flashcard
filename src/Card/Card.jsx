596a75ddc042eea4dfd370ee77ff8ef71054a367
Neveo Harrison <neveo@Neveos-MacBook-Pro.local>
2017-12-06 12:50:51 -0500

    Adding Card Coponent and changing the initial App.js file.

---
+++ src/Card/Card.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+const Card = (props) =>(
+	<div className="card-container">
+		<div className="card">
+			<div className="front">
+				<div className="eng">English Word</div>
+			</div>
+			<div className="back">
+				<div className="han">Hanzi</div>
+				<div className="pin">Pinyin</div>
+			</div>
+		</div>
+	</div>
+)
+
+export default Card
